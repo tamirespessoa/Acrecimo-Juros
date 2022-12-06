cb9ec1d036be3b0e8a6ca6d0b65a199c7ee841ca
Tamires Sousa Pessoa <109546094+tamirespessoa@users.noreply.github.com>
2022-12-06 13:41:05 -0300

    Add files via upload

---
+++ aula.js
@@ -0,0 +1,9 @@
+
+
+function incrementarJuros(valor, percentualJuros) {
+    const valorDeAcrecimo =(percentualJuros / 100) * valor;
+    return valor + valorDeAcrecimo;
+}
+
+console.log(incrementarJuros(60, 10));
+
